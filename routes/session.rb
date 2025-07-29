b1bd0a165506106bed2fd74d73eacb5748c67042
louis <louis@machin.dev>
2025-07-29 21:13:33 +0100

    Added logging on /logout

--- routes/session.rb
+++ routes/session.rb
@@ -36,8 +36,12 @@ post '/login' do
 end
 
 get '/logout' do
+  puts "Request host: #{request.host_with_port}"
+  puts "Request scheme: #{request.scheme}"
   token = request.cookies[$env.cookie_name]
   $env.given_tokens.delete(token) if token
   response.delete_cookie($env.cookie_name, path: '/')
-  redirect '/'
+  redirect_url = "#{request.scheme}://#{request.host_with_port}/"
+  puts "Redirecting to: #{redirect_url}"
+  redirect redirect_url
 end
