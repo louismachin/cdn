c118484c27f24059edf2ee432403583eb6a13172
louis <louis@machin.dev>
2025-08-22 12:05:14 +0100

    Adding /list path

---
+++ routes/list.rb
@@ -0,0 +1,16 @@
+get '/list/*' do
+    file_path = URI.decode_www_form_component(params['splat'][0])
+    full_path = File.join('data', file_path)
+    initial_dir = file_path.split('/')[0]
+    is_public = initial_dir == 'public'
+
+    protected! unless is_public
+
+    if File.directory?(full_path)
+        files = Dir[APP_ROOT + full_path + '/*']
+            .map { |dir| dir.gsub(APP_ROOT + '/data/', '') }
+        { success: true, files: files }.to_json
+    else
+        { success: false, files: [] }.to_json
+    end
+end
