fd2c5de168f46482956897c478e5404274ac124b
Louis <louis@machin.dev>
2025-09-15 12:32:50 +0100

    Adding key-value info support

--- routes/info.rb
+++ routes/info.rb
@@ -17,3 +17,35 @@ get '/info/*' do
         halt 400, "Invalid file type"
     end
 end
+
+post '/info/*' do
+    file_path = URI.decode_www_form_component(params['splat'][0])
+    full_path = File.join('data', file_path)
+    initial_dir = file_path.split('/')[0]
+    is_public = initial_dir == 'public'
+    protected! unless is_public
+    
+    unless File.exist?(full_path)
+        halt 404, "File or directory not found"
+    end
+    
+    unless File.file?(full_path)
+        halt 400, "Invalid file type"
+    end
+
+    request.body.rewind
+    data = JSON.parse(request.body.read)
+
+    unless data.is_a?(Hash)
+        halt 400, "Request body must be a JSON object"
+    end
+
+    if write_key_values(full_path, data)
+        content_type 'application/json'
+        { success: true, message: "File updated successfully" }.to_json
+    else
+        halt 500, "Failed to write file"
+    end
+rescue JSON::ParserError
+    halt 400, "Invalid JSON in request body"
+end
