3feffd466d6834f9be490c532044a75b057ccb86
louis <louis@machin.dev>
2025-09-21 14:38:34 +0100

    Added /upload_text route

--- routes/upload.rb
+++ routes/upload.rb
@@ -48,7 +48,7 @@ post '/upload_text/?*' do
     is_public = initial_dir == 'public'
     protected! unless is_public
 
-    unless params[:file] && params[:file][:tempfile]
+    unless params[:filename]
         halt 400, { 'success' => false, 'error' => 'Missing required fields' }.to_json
     end
 
