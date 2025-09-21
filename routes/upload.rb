4b821f29de61074fc38ce6d2344a54d1b5411fbb
louis <louis@machin.dev>
2025-09-21 15:03:30 +0100

    Added /upload_text route

--- routes/upload.rb
+++ routes/upload.rb
@@ -9,43 +9,6 @@ get '/upload/?*' do
     erb :upload, locals: { copy: @copy, key: @key }
 end
 
-post '/upload/?*' do
-    directory_path = params['splat'] ? URI.decode_www_form_component(params['splat'][0]) : ''
-    initial_dir = directory_path.split('/')[0] if !directory_path.empty?
-    is_public = initial_dir == 'public'
-    protected! unless is_public
-
-    unless params[:file] && params[:file][:tempfile]
-        halt 400, {
-            'success' => false,
-            'error' => 'Missing required fields',
-            'debug' => 'POST /upload/?*',
-        }.to_json
-    end
-
-    # Get the uploaded file
-    uploaded_file = params[:file][:tempfile]
-    filename = params[:file][:filename]
-
-    # Build the full path: directory from splat + actual filename
-    if directory_path.empty?
-        full_path = File.join('data', filename)
-    else
-        full_path = File.join('data', directory_path, filename)
-    end
-
-    # Ensure the directory exists
-    directory = File.dirname(full_path)
-    FileUtils.mkdir_p(directory) unless Dir.exist?(directory)
-
-    # Save the file
-    File.open(full_path, 'wb') { |f| f.write(uploaded_file.read) }
-
-    clear_file_tree_cache
-    content_type 'application/json'
-    { :success => true }.to_json
-end
-
 post '/upload_text/?*' do
     directory_path = params['splat'] ? URI.decode_www_form_component(params['splat'][0]) : ''
     initial_dir = directory_path.split('/')[0] if !directory_path.empty?
@@ -78,6 +41,43 @@ post '/upload_text/?*' do
 
     # Save the file
     File.open(full_path, 'wb') { |f| f.write(content) }
+
+    clear_file_tree_cache
+    content_type 'application/json'
+    { :success => true }.to_json
+end
+
+post '/upload/?*' do
+    directory_path = params['splat'] ? URI.decode_www_form_component(params['splat'][0]) : ''
+    initial_dir = directory_path.split('/')[0] if !directory_path.empty?
+    is_public = initial_dir == 'public'
+    protected! unless is_public
+
+    unless params[:file] && params[:file][:tempfile]
+        halt 400, {
+            'success' => false,
+            'error' => 'Missing required fields',
+            'debug' => 'POST /upload/?*',
+        }.to_json
+    end
+
+    # Get the uploaded file
+    uploaded_file = params[:file][:tempfile]
+    filename = params[:file][:filename]
+
+    # Build the full path: directory from splat + actual filename
+    if directory_path.empty?
+        full_path = File.join('data', filename)
+    else
+        full_path = File.join('data', directory_path, filename)
+    end
+
+    # Ensure the directory exists
+    directory = File.dirname(full_path)
+    FileUtils.mkdir_p(directory) unless Dir.exist?(directory)
+
+    # Save the file
+    File.open(full_path, 'wb') { |f| f.write(uploaded_file.read) }
 
     clear_file_tree_cache
     content_type 'application/json'
