4bed8dce0845187dbcc124694d7ca9a17b8ccffd
louismachin <louis@machin.dev>
2026-07-19 02:59:47 +0100

    Migrating UI to modals

--- routes/view.rb
+++ routes/view.rb
@@ -11,16 +11,22 @@ get '/view' do
         key: @key,
         is_jailed: @is_jailed,
         all_dirs: @all_dirs,
+        read_only: false,
     }
 end
 
 get '/view/*' do
-    protected!
-    @copy = $env.default_copy
     file_path = URI.decode_www_form_component(params['splat'][0])
-    @key = file_path.split('/')
+    key = file_path.split('/')
+    initial_dir = key[0]
+    is_public = initial_dir == 'public'
+
+    protected! unless is_public
+
+    @copy = $env.default_copy
+    @key = key
     @all_dirs = collect_all_dirs(get_file_tree)
-    @is_jailed = false
+    @is_jailed = is_public # public visitors shouldn't browse above /public
     @file_tree = get_file_tree(@key)
     erb :home, locals: {
         copy: @copy,
@@ -28,5 +34,6 @@ get '/view/*' do
         key: @key,
         is_jailed: @is_jailed,
         all_dirs: @all_dirs,
+        read_only: !logged_in?, # or however you currently check session state
     }
 end
